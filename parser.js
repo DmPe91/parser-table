c8a291ab69f94a489263616b58dee2116335ceb7
DmPe91 <dmper67@gmail.com>
2024-03-17 23:45:30 +0300

    2commit

--- parser.js
+++ parser.js
@@ -1,7 +1,6 @@
 const puppeteer = require("puppeteer");
-const Blob = require("node-blob/index");
 const fs = require("fs");
-var FileSaver = require("file-saver");
+let parse = require("csv-parse");
 
 const url = "https://www.rp-ural.ru/stock/";
 
@@ -11,20 +10,40 @@ const url = "https://www.rp-ural.ru/stock/";
   await page.goto(url);
   let arr = await page.evaluate(() => {
     let text = Array.from(
-      document.querySelectorAll("table"),
+      document.querySelectorAll("tr"),
       (el) => el.innerText
     );
     return text;
   });
-  let blob = new Blob([arr], { type: "text/csv;charset=utf-8;" });
-  fs.writeFile("data.csv", [arr], function (error) {
-    if (error) {
-      // если ошибка
-      return console.log(error);
-    }
-    console.log("Файл успешно записан");
+
+  let table_arr = arr.map(function (el) {
+    el = el.split("\n");
+    el = el.map((element) => {
+      element = element.trimStart();
+      return element;
+    });
+    el = el.filter((num) => {
+      return num !== "";
+    });
+    el[el.length - 1] = el[el.length - 1] + "\n";
+
+    return el;
   });
-  ///let file_csv = FileSaver.saveAs(blob, "data.csv");
+  ///for (let i = 0; i < table_arr.length; i++)
+  table_arr.forEach((elem) => {
+    for (let i = 0; i < elem.length; i++) {
+      fs.writeFileSync(
+        "table.csv",
+        elem[i] + ",",
+        { flag: "a" },
+        function (error) {
+          if (error) throw error; // ошибка чтения файла, если есть
+          console.log("Данные успешно записаны записать файл");
+        }
+      );
+    }
+  });
+  ///console.log(table_arr);
 
   await browser.close();
 })();
